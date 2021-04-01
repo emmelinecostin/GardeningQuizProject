9dbdfcb9315804d32552d396ff79f13c9d7f9883
Emmeline Costin <emmecostin@gmail.com>
2021-03-31 21:04:46 -0700

    First Commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const correctAnswers = ["B", "D", "B", "C", "A", "D", "D", "A", "A", "D"];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+const response = document.querySelector('.response');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let score = 0;
+    const formAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value,
+    form.q6.value, form.q7.value, form.q8.value, form.q9.value, form.q10.value,];
+
+    // Comparing answers and changing the score
+    formAnswers.forEach((answer, index) => {
+        if (answer === correctAnswers[index]) {
+            score += 10;
+        }
+    });
+
+    // Results 
+    scrollTo(0, 0);
+    result.querySelector('span').textContent = `${score}%`;
+    result.classList.remove('d-none')
+
+    if (score >= 80) {
+        result.querySelector('.great').textContent = `You've got a green thumb!`
+    } else {
+        result.querySelector('.great').textContent = `Don't worry, with a little bit of learning you will be an expert!`
+    }
+
+});
